b3b6125f99ac5ce2ea6a53e8fae3f045cd350285
Ayaminn <ayamin.imai@gmail.com>
2018-12-19 18:24:01 +0900

    いんすたんてぃえーと

---
+++ Assets/BallControll.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControll : MonoBehaviour
+{ 
+    //ボールというオブジェクトを紐付ける
+    public GameObject ball;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(ball);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
