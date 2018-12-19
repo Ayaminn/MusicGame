963249c7b04f3a8a7698cea9438feea46a855a68
Ayaminn <ayamin.imai@gmail.com>
2018-12-19 18:56:24 +0900

    ボールのそくどかえた、ボールでてくる位置決めた、シーンの名前きめた

--- Assets/BallControll.cs
+++ Assets/BallControll.cs
@@ -13,6 +13,6 @@ public class BallControll : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(0, -1, 0);
+        transform.position += new Vector3(0, -0.2f, 0);
     }
 }
--- Assets/BallLauncher.cs
+++ Assets/BallLauncher.cs
@@ -10,7 +10,7 @@ public class BallLauncher : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(ball);
+        Instantiate(ball, new Vector3(0, 10, 0), Quaternion.identity);
     }
 
     // Update is called once per frame
